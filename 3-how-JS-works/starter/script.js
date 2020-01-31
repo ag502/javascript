2ab6b23d7f8ca8166097fbc29a90314c762689d7
ag502 <ag502@naver.com>
2020-02-01 01:56:57 +0900

    Practice JavaScript Behind the Scene
    Practice JavaScript Hoising

---
+++ 3-how-JS-works/starter/script.js
@@ -0,0 +1,84 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+calculateAge(1995);
+
+function calculateAge(year) {
+    console.log(2020 - year + 1);
+}
+
+// retirement(1990);
+var retirement = function(year) {
+    console.log(65 - (2020 - year));
+}
+
+// variables
+// console.log(age);
+var age = 23;
+console.log(age);
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
