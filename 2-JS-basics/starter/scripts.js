0fb234e47410ba4c0e1797e4bde3598d52096d7a
ag502 <ag502@naver.com>
2020-01-31 16:15:02 +0900

    Practice JavaScript Language Basic
    Practice JavaScript Function Statement and Declaration

--- 2-JS-basics/starter/scripts.js
+++ 2-JS-basics/starter/scripts.js
@@ -29,3 +29,30 @@ yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1969, 'Jane');
 */
+
+/*****************************
+* Function Statements and Expressions
+*/
+
+/*
+// Function declaration
+// function whatDoYouDo(job, firstName) {}
+
+// Function Expression
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+        case 'dirver':
+            return firstName + ' dirves a cab in Lisbon.';
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
+*/
