d27915b8980a8031d35849375e96a7cdfd18e7fb
ag502 <ag502@naver.com>
2020-01-31 21:34:25 +0900

    Practice JavaScript Language Basic
    Practice JavaScript Object and Method

--- 2-JS-basics/starter/scripts.js
+++ 2-JS-basics/starter/scripts.js
@@ -133,6 +133,7 @@ console.log('Tips: ', allThreeTips, 'Total Paid: ', finalPaidAmounts);
 * Objects and properties
 */
 
+/*
 // Objerct literal
 var john = {
     firstName: 'John',
@@ -158,3 +159,24 @@ jane.firstName = 'Jane';
 jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
+*/
+
+/*****************************
+* Objects and methods
+*/
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2020 - this.birthYear; //john.birthYear
+    }
+};
+
+// john.age = john.calcAge();
+john.calcAge();
+console.log(john);
