b319547301597a2ca0c580768847a41d5527bcd7
ag502 <ag502@naver.com>
2020-02-01 12:46:13 +0900

    Practice JavaScript Behind the Scene
    Practice JavaScript 'this' Keyword

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -51,7 +51,7 @@ function first() {
 
 // Example to show the differece between execution stack and scope chain
 
-
+/*
 var a = 'Hello!';
 first();
 
@@ -69,16 +69,48 @@ function third() {
     var d = 'John';
     console.log(a + b + c + d);
 }
-
+*/
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+console.log(this);
 
+// In a regular function call, this always point to the window object
+calculateAge(1995);
 
+function calculateAge(year) {
+    console.log(2020 - year + 1);
+    console.log(this);
+}
 
+var john = {
+    name : 'John',
+    yearOfBirth : 1990,
+    calculateAge : function() {
+        console.log(this);
+        console.log(2020-this.yearOfBirth+1);
+
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    } 
+};
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
 
 
