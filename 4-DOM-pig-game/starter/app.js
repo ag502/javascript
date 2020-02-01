a0b8aa781563cb68d9fe623f6da944e101a30aba
ag502 <ag502@naver.com>
2020-02-01 18:24:49 +0900

    Generate Random Number for Dice
    Modify Dice Number in HTML Page by using DOM

---
+++ 4-DOM-pig-game/starter/app.js
@@ -0,0 +1,20 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/ 
+
+var scores, roundScores, activePlayer, dice;
+
+scores = [0, 0];
+roundScores = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector("#current-" + activePlayer).textContent = dice;
