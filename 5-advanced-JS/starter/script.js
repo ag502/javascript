3b6a557df39825fb0806c21391f04cc739a647b0
ag502 <ag502@naver.com>
2020-02-02 21:01:14 +0900

    Practice JavaScript Objects and Functions
    Compare Primitives and Objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -31,6 +31,7 @@ console.log(john.lastName, jane.lastName, mark.lastName);
 
 // Object.create
 
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2020 - this.yearOfBirth);
@@ -47,3 +48,40 @@ var jane = Object.create(personProto, {
     yearOfBirth : {value: 1969},
     job: {value: 'designer'}
 });
+*/
+
+// Primitives vs Objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects 
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbone',
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'Sans Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
