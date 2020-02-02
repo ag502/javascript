7deb689bfbc83fb4c951628a8813ca22614246a4
ag502 <ag502@naver.com>
2020-02-02 18:54:50 +0900

    Practice JavaScript Objects and Function
    Practice JavaScript Creating Objects(Function Constructor)

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,28 @@
+// Function constructor
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher',
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2020 - this.yearOfBirth + 1);
+}
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName, jane.lastName, mark.lastName);
