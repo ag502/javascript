596218ced1f6b98e6691adb93e88d5c7119663d5
ag502 <ag502@naver.com>
2020-02-02 21:18:13 +0900

    Practice JavaScript Objects and Functions
    Practice JavaScript First Class Functions : Passing Functions as Arguments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -52,6 +52,7 @@ var jane = Object.create(personProto, {
 
 // Primitives vs Objects
 
+/*
 // Primitives
 var a = 23;
 var b = a;
@@ -85,3 +86,41 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
+
+// First Class Functions
+//// Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+} 
+
+function calculateAge(el) {
+    return 2020 - el + 1;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }   
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
