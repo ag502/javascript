0cddf3126b1cf3b16ff141aeb6ccaf9514737527
ag502 <ag502@naver.com>
2020-02-02 21:30:29 +0900

    Practice JavaScript Objects and Function
    Practice JavaScript First Class Function: Function Returning Function

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -91,6 +91,7 @@ console.log(obj.city);
 // First Class Functions
 //// Passing functions as arguments
 
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -124,3 +125,30 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+//// Functions Returning Functions
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Mark');
