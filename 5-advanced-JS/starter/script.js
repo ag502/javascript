3f57169d96ac6a9f39c2f930737d64645afaa7ea
ag502 <ag502@naver.com>
2020-02-03 12:13:29 +0900

    Practice JavaScript Objects and Function
    Practice Coding Challenge

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -213,6 +213,7 @@ interviewQuestion('teacher')('John');
 
 // Bind, Call, Apply
 
+/*
 var john = {
     name: 'John',
     age: 26,
@@ -268,3 +269,83 @@ var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 
 console.log(ages);
 console.log(fullJapan);
+*/
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+function Question(question, answers, answer) {
+    this.question = question;
+    this.answers = answers;
+    this.answer = answer;
+}
+
+var firstQuestion = new Question("What is my Name", ['1. Lee', '2. Kim'], '1');
+var secondQuestion = new Question("The place Where I'm Working", ['1. Naver', '2. Kakao', '3. Yanolja'], '1');
+var thirdQuestion = new Question("What is my Job?", ['1. Developer', '2. Pharmacist'], '1');
+
+var questions = [firstQuestion, secondQuestion, thirdQuestion];
+
+(function () {
+    var answer = generateQuestion(questions);
+    var input = prompt('Input the Answer');
+    isCorrect(input, answer);
+
+    function generateQuestion(questions) {
+        var randNum = Math.floor(Math.random() * questions.length);
+        var selectedQuestion = questions[randNum];
+        
+        console.log(selectedQuestion.question);
+    
+        for (var i = 0; i < selectedQuestion.answers.length; i++) {
+            console.log(selectedQuestion.answers[i]);
+        }
+    
+        return selectedQuestion.answer;
+    }
+    
+    function isCorrect(input, answer) {
+        if (input === answer) {
+            console.log('correct');
+        } else {
+            console.log('wrong!!');
+        }
+    }
+})();
+
+
+
+// generateQuestion(questions);
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
