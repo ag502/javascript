e113154f5f0b4ffcefee295f2aa908b575e8e33c
ag502 <ag502@naver.com>
2020-02-08 15:14:52 +0900

    Practice JavaScript ES6
    Practice ES6 Default Parameters

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -353,6 +353,7 @@ function isFullAge6(...years) {
 isFullAge6(1990, 1999, 1965);
 */
 
+/*
 // ES5
 function isFullAge5(limit) {
     // console.log(arguments);
@@ -373,3 +374,33 @@ function isFullAge6(limit, ...years) {
 }
 
 isFullAge6(30, 1990, 1999, 1965);
+*/
+
+// Lecture: Default Parameters
+
+// ES5
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+    lastName === undefined ? lastName = 'Smith' : lastName;
+    nationality === undefined ? nationality = 'American' : nationality;
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
+*/
+
+// ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'American') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
