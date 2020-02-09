ec7bcc61167defce141538c3be85e427399f07ef
ag502 <ag502@naver.com>
2020-02-09 16:33:30 +0900

    Practice JavaScript Asynchronous
    Practice JavaScript Async Await

--- 8-asynchronous-JS/starter/scripts.js
+++ 8-asynchronous-JS/starter/scripts.js
@@ -47,6 +47,7 @@ getRecipe();
 
 // From CallBack Hell to Promise
 
+/*
 const getIDS = new Promise((resolve, reject) => {
     setTimeout(() => {
          resolve([523, 883, 432, 974]);
@@ -92,3 +93,53 @@ getIDS
 .catch(error => {
     console.log('ERROR!');
 });
+*/
+
+// From Promise to Async/Await
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+         resolve([523, 883, 432, 974]);
+    }, 1500)
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {
+                title: 'fresh tomato pasta',
+                publisher: 'Jonas'
+            };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recID);
+    });
+}
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {
+                title: 'Italian Pizza',
+                publisher: 'Jonas'
+            }
+            resolve(`${pub}: ${recipe.title}`);
+        }, 1500, publisher)
+    })
+};
+
+async function getRecipeAW() {
+    const IDs = await getIDs;
+    console.log(IDs);
+
+    const recipe = await getRecipe(IDs[2]);
+    console.log(recipe);
+
+    const related = await getRelated('Johnas');
+    console.log(related);
+
+    return recipe;
+}
+
+getRecipeAW().then(result => console.log(result));
+
+// console.log(rec);
