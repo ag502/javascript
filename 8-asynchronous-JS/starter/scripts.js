c350b522c093728ee8286103fb4c7669ba92a167
ag502 <ag502@naver.com>
2020-02-09 17:41:44 +0900

    Practice JavaScript Asynchronous
    Practice JavaScript AJAX with fetch, promise

--- 8-asynchronous-JS/starter/scripts.js
+++ 8-asynchronous-JS/starter/scripts.js
@@ -97,6 +97,7 @@ getIDS
 
 // From Promise to Async/Await
 
+/*
 const getIDs = new Promise((resolve, reject) => {
     setTimeout(() => {
          resolve([523, 883, 432, 974]);
@@ -143,3 +144,25 @@ async function getRecipeAW() {
 getRecipeAW().then(result => console.log(result));
 
 // console.log(rec);
+*/
+
+// AJAX and APIs
+
+function getWeather(woeid) {
+    fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+    .then(result => {
+        console.log(result);
+        return result.json();
+    })
+    .then(data => {
+        // console.log(data);
+        const today = data.consolidated_weather[0]
+        console.log(`temperature in ${data.title} stay between ${today.min_temp} and ${today.max_temp}`);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
+getWeather(2487956);
+getWeather(44418);
