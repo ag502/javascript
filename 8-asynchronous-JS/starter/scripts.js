3d47a791fea2e80d3e738495cb2c9dbe7394efe2
ag502 <ag502@naver.com>
2020-02-09 15:34:36 +0900

    Practice JavaScript Asynchronous
    Practice Promise

--- 8-asynchronous-JS/starter/scripts.js
+++ 8-asynchronous-JS/starter/scripts.js
@@ -18,6 +18,7 @@ first();
 
 // The Old Way: Asynchronous JavaScript with CallBacks
 
+/*
 function getRecipe() {
     setTimeout(() => {
         const recipeID = [523, 883, 432, 974];
@@ -42,3 +43,52 @@ function getRecipe() {
 }
 
 getRecipe();
+*/
+
+// From CallBack Hell to Promise
+
+const getIDS = new Promise((resolve, reject) => {
+    setTimeout(() => {
+         resolve([523, 883, 432, 974]);
+    }, 1500)
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {
+                title: 'fresh tomato pasta',
+                publisher: 'Jonas'
+            };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recID);
+    });
+}
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {
+                title: 'Italian Pizza',
+                publisher: 'Jonas'
+            }
+            resolve(`${pub}: ${recipe.title}`);
+        }, 1500, publisher)
+    })
+};
+
+getIDS
+.then(IDs => {
+    console.log(IDs);
+    return getRecipe(IDs[2]);
+})
+.then(recipe => {
+    console.log(recipe);
+    return getRelated('Jonas');
+})
+.then(recipe => {
+    console.log(recipe);
+})
+.catch(error => {
+    console.log('ERROR!');
+});
