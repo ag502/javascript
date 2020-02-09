e8a0d7f43e396219020621b4e200abe5c9af2f16
ag502 <ag502@naver.com>
2020-02-09 12:50:22 +0900

    Practice JavaScript Asynchronous
    The old way to process Asynchronous

--- 8-asynchronous-JS/starter/scripts.js
+++ 8-asynchronous-JS/starter/scripts.js
@@ -1,3 +1,6 @@
+// Examples of Asynchronous
+
+/*
 const second = () => {
     setTimeout(() => {
         console.log('Async Hey there');
@@ -11,3 +14,31 @@ const first = () => {
 }
 
 first();
+*/
+
+// The Old Way: Asynchronous JavaScript with CallBacks
+
+function getRecipe() {
+    setTimeout(() => {
+        const recipeID = [523, 883, 432, 974];
+        console.log(recipeID);
+
+        setTimeout((id) => {
+            const recipe = {
+                title: 'fresh tomato pasta',
+                publisher: 'Jonas'
+            }
+            console.log(`${id}: ${recipe.title}`);
+
+            setTimeout(publisher => {
+                const recipe = {
+                    title: 'Italian Pizze',
+                    publisher: 'Jonas'
+                }
+                console.log(recipe);
+            }, 1500, recipe.publisher)
+        }, 1500, recipeID[2]);
+    }, 1500);
+}
+
+getRecipe();
