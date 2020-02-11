a2761b742cf9d3523b51173daf42fbb9fe431edd
ag502 <ag502@naver.com>
2020-02-11 20:49:37 +0900

    Practice WebPack
    Practice WebPack Loader

--- 10-webpack/src/index.js
+++ 10-webpack/src/index.js
@@ -1,4 +1,7 @@
 import hello from './hello.js';
 import world from "./world.js";
+import css from './style.css';
 
 document.querySelector('#root').innerHTML = hello + world;
+
+console.log(css);
--- 10-webpack/webpack.config.js
+++ 10-webpack/webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
         path: path.resolve(__dirname, "public"),
         filename: 'index_bundle.js'
     },
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
+    }
 }
