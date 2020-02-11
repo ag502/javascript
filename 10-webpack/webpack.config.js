53f254ed9d2bc36f78b7d3b15bffdd14cd64f892
ag502 <ag502@naver.com>
2020-02-11 21:26:42 +0900

    Practice WebPack
    Practice WebPack plugins

--- 10-webpack/webpack.config.js
+++ 10-webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -20,5 +21,17 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            chunks: ['index']
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/about.html',
+            filename: './about.html',
+            chunks: ['about']
+        })
+    ]
 }
