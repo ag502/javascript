dec724873cfa3a85aaad3db69b783405913a6e15
ag502 <ag502@naver.com>
2020-02-14 22:31:27 +0900

    Mordern JavaScript Project
    Practice way of importing modules in ES6

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,4 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module test.js! Variable x is ${x}`);
+import str from "./models/Search";
+import {add as a, multiply as m, ID} from "./views/searchView"
+// import * as searchView from './views/searchView'
+console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}.`)
