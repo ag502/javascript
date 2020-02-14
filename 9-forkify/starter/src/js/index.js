2da9e904eccba8e8a3b9d66e3255dae2b9c9d3d1
ag502 <ag502@naver.com>
2020-02-15 00:05:12 +0900

    Mordern JavaScript Project
    Design Search Controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,33 @@
 import Search from './models/Search'
 
-const search = new Search('pizza');
+/** Global state of the app
+ * - Search object
+ * - Current recipe object4
+ * - Shopping list object
+ * - Liked recipe
+ */
 
-console.log(search);
-search.getResults().then(() => console.log(search.recipe));
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza'
+
+    if (query) {
+        // 2) New Search object and add to State
+        state.search = new Search(query);
+
+        // 3) PrePare UI for result
+
+        // 4) Search for recipe
+        await state.search.getResults();
+
+        // 5) render results on UI
+        console.log(state.search.recipe);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
