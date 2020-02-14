f046abeb956f603f835db6a49b6e7e4dddca62fc
ag502 <ag502@naver.com>
2020-02-14 21:23:30 +0900

    Practice WebPack

--- 10-webpack/src/index.js
+++ 10-webpack/src/index.js
@@ -1,6 +1,7 @@
 import hello from './hello.js';
 import world from "./world.js";
 import css from './style.css';
+import _ from "lodash";
 
 document.querySelector('#root').innerHTML = hello + world;
 
