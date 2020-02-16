13f567eb761e18137cc3fc0611e244f82bc709f0
ag502 <ag502@naver.com>
2020-02-16 16:38:48 +0900

    Modern JavaScript Project

---
+++ 9-forkify/starter/src/js/models/List.js
@@ -0,0 +1,15 @@
+import uniqid from 'uniqid'
+
+export default class List {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(count, unit, ingredient) {
+
+    }
+
+    deleteItem(id) {
+        // this.item.splice()
+    }
+}
